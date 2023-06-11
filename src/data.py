08d8d8ce8ada8426584f5296406afb99712387c1
Maria Wyrzykowska <maryswyrz@gmail.com>
2023-06-11 11:52:52 +0200

    Refactor UNet training notebook

--- src/data.py
+++ src/data.py
@@ -7,14 +7,16 @@ from torch.utils.data import IterableDataset
 from typing import Union, Optional
 from pathlib import Path
 
+
 class RandomPatchesDataset(IterableDataset):
     def __init__(
-        self,
-        images_dir: Union[Path, str],
-        masks_dir: Union[Path, str],
-        patch_size: tuple[int, int] = (256, 256),
-        seed: Optional[Union[np.random.Generator, int]] = None,
-        ) -> None:
+            self,
+            images_dir: Union[Path, str],
+            masks_dir: Union[Path, str],
+            patch_size: tuple[int, int] = (256, 256),
+            seed: Optional[Union[np.random.Generator, int]] = None,
+            max_iter_in_epoch: Optional[int] = 50,
+    ) -> None:
 
         super().__init__()
         self.images_dir = Path(images_dir)
@@ -23,37 +25,44 @@ class RandomPatchesDataset(IterableDataset):
         self.image_names = [file.name for file in self.images_dir.glob('*.tif')]
 
         print(f'Succesfully loaded {len(self.image_names)} images')
-        
+
         self.patch_size = patch_size
         self.rng = np.random.default_rng(seed)
+        self._i = 0
+        self._max_iter = max_iter_in_epoch
 
     def __next__(self) -> np.ndarray:
+        if self._i > self._max_iter_in_epoch:
+            self._i = 0
+            raise StopIteration
         random_name = self.rng.choice(self.image_names)
         random_image = imageio.imread(self.images_dir / random_name)
         random_mask = imageio.imread(self.masks_dir / random_name)
 
         if len(random_image.shape) == 3:
             # Some images have three channels instead of one, but they all contain the same values
-            assert(
-                np.all(random_image[:, :, 0] == random_image[:, :, 1])
-                and np.all(random_image[:, :, 0] == random_image[:, :, 2])
+            assert (
+                    np.all(random_image[:, :, 0] == random_image[:, :, 1])
+                    and np.all(random_image[:, :, 0] == random_image[:, :, 2])
             )
             random_image = random_image[:, :, 0]
-        
+
         crop_y = np.random.randint(0, random_image.shape[0] - self.patch_size[0])
         crop_x = np.random.randint(0, random_image.shape[1] - self.patch_size[1])
 
         random_image_crop = random_image[
-            crop_y: crop_y + self.patch_size[0],
-            crop_x: crop_x + self.patch_size[1],
-            ]
+                            crop_y: crop_y + self.patch_size[0],
+                            crop_x: crop_x + self.patch_size[1],
+                            ]
         random_mask_crop = random_mask[
-            crop_y: crop_y + self.patch_size[0],
-            crop_x: crop_x + self.patch_size[1],
-            ]
+                           crop_y: crop_y + self.patch_size[0],
+                           crop_x: crop_x + self.patch_size[1],
+                           ]
 
         random_mask_crop[random_mask_crop > 0] = 1
-        
+
+        self._i += 1
+
         return (random_image_crop[np.newaxis, ...].astype(float),
                 random_mask_crop[np.newaxis, ...].astype(float))
 
