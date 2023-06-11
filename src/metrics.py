7aee089a2d316a5a70941806b218787491e6ef87
Adrian Urbanski <adrianu161999@gmail.com>
2023-06-11 13:55:13 +0200

    Add mapping of instances and evaluation

--- src/metrics.py
+++ src/metrics.py
@@ -2,25 +2,24 @@ import numpy as np
 
 def calculate_dice(mask_true, mask_pred, eps=1e-5):
     # cast to binary
-    mask_true[mask_true > 0] = 1
-    mask_pred[mask_pred > 0] = 1
+    mask_true = mask_true.astype(bool)
+    mask_pred = mask_pred.astype(bool)
 
     intersection = np.sum(mask_true & mask_pred)
     return (2 * intersection) / (np.sum(mask_true) + np.sum(mask_pred) + eps)
 
 def calculate_iou(mask_true, mask_pred, eps=1e-5):
     # cast to binary
-    mask_true[mask_true > 0] = 1
-    mask_pred[mask_pred > 0] = 1
+    mask_true = mask_true.astype(bool)
+    mask_pred = mask_pred.astype(bool)
 
     intersection = np.sum(mask_true & mask_pred)
     union = np.sum(mask_true | mask_pred)
     return intersection / (union + eps)
 
 def calculate_map(mask_true, mask_pred, eps=1e-5):
-    # może jednak nie powinniśmy binaryzowac maski jeśli chcemy to liczyć?
     precision = 0
-    classes = np.unique(mask_true)
+    classes = np.unique(np.concatenate((mask_true, mask_pred)))
     for class_idx in classes:
         precision += (
             np.sum((mask_true == class_idx) & (mask_pred == class_idx))
--- src/utils.py
+++ src/utils.py
@@ -3,13 +3,24 @@ import numpy as np
 
 from skimage.segmentation import watershed
 from skimage.feature import peak_local_max
+from stardist.matching import matching
 
 
-def apply_watershed(semantic_mask: np.ndarray) -> np.ndarray:
+def apply_watershed(semantic_mask: np.ndarray, min_distance: int = 30) -> np.ndarray:
     distance = ndi.distance_transform_edt(semantic_mask)
-    coords = peak_local_max(distance, footprint=np.ones((3,3)), labels=semantic_mask)
+    coords = peak_local_max(distance, labels=semantic_mask, min_distance=min_distance)
 
     mask = np.zeros(distance.shape, dtype=bool)
     mask[tuple(coords.T)] = True
     markers, _ = ndi.label(mask)
     return watershed(-distance, markers, mask=semantic_mask)
+
+
+def map_mask(mask, pred):
+    matches = matching(mask.astype(int), pred, report_matches=True)
+    mapping = np.array(matches.matched_pairs)
+    max_mask_class = np.max(mapping[:, 0])
+    pred[pred != 0] += max_mask_class
+    for mask_val, pred_val in mapping:
+        pred[pred == pred_val + max_mask_class] = mask_val
+    return pred
